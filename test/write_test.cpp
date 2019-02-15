ab7a798154ff736d1824533fc08ebf4f1ed5a993
Alex Liu <liu.alex.c@gmail.com>
2019-02-15 09:34:55 -0500

    Small change to test

--- test/write_test.cpp
+++ test/write_test.cpp
@@ -11,11 +11,10 @@ int main() {
     string file = "/home/cz3liu/courses/cs798/project2/Simple-Network-Filesystem/tmp/testfile";
 
     int fd = open(file.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0666);
-
     for (int i = 0; i < 1000; i++) {
         cout << i << endl;
-        string toWrite =  to_string(i) + "\n";
-        write(fd, toWrite.c_str(), toWrite.length());
+        string numStr =  to_string(i) + "\n";
+        write(fd, numStr.c_str(), numStr.length());
         sleep(1);
     }
 
